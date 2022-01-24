88863d10b2fc55dcc423ef9e7f663e5563e2b504
Blue Kirby <blue.kirby@tutanota.com>
2022-01-24 15:41:42 -0300

    fix: add id and signature to tx_receipt

--- src/verify/types.rs
+++ src/verify/types.rs
@@ -3,7 +3,8 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Item {
-    pub id: String,
+    pub tx_id: String,
+    pub signature: Vec::<u8>
 }
 
 pub struct Header(pub u64, pub String);
