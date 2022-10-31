3d01dfa9412140e46e4aeba8fe988454a40bfe52
Blue Kirby <blue.kirby@tutanota.com>
2022-10-31 00:49:26 +0000

    feat: enhance cli help and commands

--- src/client/bin/cli.rs
+++ src/client/bin/cli.rs
@@ -1,110 +1,227 @@
-use std::{pin::Pin, time::Duration};
+use std::time::Duration;
 
 use bundlr_sdk::{
     client::{
-        balance::run_balance, fund::run_fund, method::Method, price::run_price, upload::run_upload,
+        balance::run_balance, fund::run_fund, price::run_price, upload::run_upload,
         withdraw::run_withdraw,
     },
     currency::CurrencyType,
-    error::BundlrError,
 };
-use clap::Parser;
-use futures::Future;
+use clap::{Parser, Subcommand};
 use reqwest::Url;
 
+const DEFAULT_BYTE_AMOUNT: u64 = 1;
 const DEFAULT_TIMEOUT: u64 = 1000 * 30; //30 secs
 const DEFAULT_TIMEOUT_FUND: u64 = 1000 * 60 * 30; //30 mins
 
-#[derive(Clone, Debug, Parser)]
-#[clap(name = "cli")]
-#[clap(about = "", long_about = None)]
+#[derive(Parser)]
 struct Args {
-    #[clap(value_parser)]
-    method: Method,
+    #[clap(subcommand)]
+    command: Command,
+}
+#[derive(Subcommand)]
+enum Command {
+    ///Gets the specified user's balance for the current Bundlr node
+    Balance {
+        //Address to query balance
+        #[clap(value_parser)]
+        address: String,
 
-    #[clap(value_parser)]
-    first_arg: Option<String>,
+        //Timeout for operation
+        #[clap(long = "timeout")]
+        timeout: Option<u64>,
 
-    #[clap(long = "timeout")]
-    timeout: Option<u64>,
+        //Host address
+        #[clap(long = "host")]
+        host: Url,
 
-    #[clap(short = 'w', long = "wallet")]
-    wallet: Option<String>,
+        //Currency type
+        #[clap(short = 'c', long = "currency")]
+        currency: CurrencyType,
+    },
+    ///Funds your account with the specified amount of atomic units
+    Fund {
+        //Amounts, in winston, to send in funding
+        #[clap(value_parser)]
+        amount: u64,
 
-    #[clap(short = 'h', long = "host")]
-    host: Url,
+        //Timeout for operation
+        #[clap(long = "timeout")]
+        timeout: Option<u64>,
 
-    #[clap(short = 'c', long = "currency")]
-    currency: CurrencyType,
+        //Path to wallet
+        #[clap(short = 'w', long = "wallet")]
+        wallet: String,
+
+        //Host address
+        #[clap(long = "host")]
+        host: Url,
+
+        //Currency type
+        #[clap(short = 'c', long = "currency")]
+        currency: CurrencyType,
+    },
+    ///Sends a fund withdrawal request
+    Withdraw {
+        //Amounts, in winston, to send in withdraw
+        #[clap(value_parser)]
+        amount: u64,
+
+        //Timeout for operation
+        #[clap(long = "timeout")]
+        timeout: Option<u64>,
+
+        //Path to wallet
+        #[clap(short = 'w', long = "wallet")]
+        wallet: String,
+
+        //Host address
+        #[clap(long = "host")]
+        host: Url,
+
+        //Currency type
+        #[clap(short = 'c', long = "currency")]
+        currency: CurrencyType,
+    },
+    ///Uploads a specified file
+    Upload {
+        //Path to file that will be uploaded
+        #[clap(value_parser)]
+        file_path: String,
+
+        //Timeout for operation
+        #[clap(long = "timeout")]
+        timeout: Option<u64>,
+
+        //Path to wallet
+        #[clap(short = 'w', long = "wallet")]
+        wallet: String,
+
+        //Host address
+        #[clap(long = "host")]
+        host: Url,
+
+        //Currency type
+        #[clap(short = 'c', long = "currency")]
+        currency: CurrencyType,
+    },
+    ///Uploads a folder (with a manifest)
+    UploadDir {},
+    ///Check how much of a specific currency is required for an upload of <amount> bytes
+    Price {
+        //Amounts of bytes to calculate pricing
+        #[clap(value_parser)]
+        byte_amount: Option<u64>,
+
+        //Timeout for operation
+        #[clap(long = "timeout")]
+        timeout: Option<u64>,
+
+        //Host address
+        #[clap(long = "host")]
+        host: Url,
+
+        //Currency type
+        #[clap(short = 'c', long = "currency")]
+        currency: CurrencyType,
+    },
+}
+
+impl Command {
+    async fn execute(self) {
+        match self {
+            Command::Balance {
+                address,
+                timeout,
+                host,
+                currency,
+            } => {
+                let work = run_balance(host, &address, &currency);
+                let timeout = timeout.unwrap_or(DEFAULT_TIMEOUT);
+                match tokio::time::timeout(Duration::from_millis(timeout), work).await {
+                    Ok(res) => match res {
+                        Ok(ok) => println!("[Ok] {}", ok),
+                        Err(err) => println!("[Err] {}", err),
+                    },
+                    Err(err) => println!("Error running task: {}", err.to_string()),
+                }
+            }
+            Command::Fund {
+                amount,
+                timeout,
+                wallet,
+                host,
+                currency,
+            } => {
+                let work = run_fund(amount, host, &wallet, currency);
+                let timeout = timeout.unwrap_or(DEFAULT_TIMEOUT_FUND);
+                match tokio::time::timeout(Duration::from_millis(timeout), work).await {
+                    Ok(res) => match res {
+                        Ok(ok) => println!("[Ok] {}", ok),
+                        Err(err) => println!("[Err] {}", err),
+                    },
+                    Err(err) => println!("Error running task: {}", err.to_string()),
+                }
+            }
+            Command::Withdraw {
+                amount,
+                timeout,
+                wallet,
+                host,
+                currency,
+            } => {
+                let work = run_withdraw(amount, host, &wallet, currency);
+                let timeout = timeout.unwrap_or(DEFAULT_TIMEOUT);
+                match tokio::time::timeout(Duration::from_millis(timeout), work).await {
+                    Ok(res) => match res {
+                        Ok(ok) => println!("[Ok] {}", ok),
+                        Err(err) => println!("[Err] {}", err),
+                    },
+                    Err(err) => println!("Error running task: {}", err.to_string()),
+                }
+            }
+            Command::Upload {
+                file_path,
+                timeout,
+                wallet,
+                host,
+                currency,
+            } => {
+                let work = run_upload(file_path, host, &wallet, currency);
+                let timeout = timeout.unwrap_or(DEFAULT_TIMEOUT);
+                match tokio::time::timeout(Duration::from_millis(timeout), work).await {
+                    Ok(res) => match res {
+                        Ok(ok) => println!("[Ok] {}", ok),
+                        Err(err) => println!("[Err] {}", err),
+                    },
+                    Err(err) => println!("Error running task: {}", err.to_string()),
+                }
+            }
+            Command::UploadDir {} => todo!(),
+            Command::Price {
+                byte_amount,
+                timeout,
+                host,
+                currency,
+            } => {
+                let byte_amount = byte_amount.unwrap_or(DEFAULT_BYTE_AMOUNT);
+                let work = run_price(host, currency, byte_amount);
+                let timeout = timeout.unwrap_or(DEFAULT_TIMEOUT);
+                match tokio::time::timeout(Duration::from_millis(timeout), work).await {
+                    Ok(res) => match res {
+                        Ok(ok) => println!("[Ok] {}", ok),
+                        Err(err) => println!("[Err] {}", err),
+                    },
+                    Err(err) => println!("Error running task: {}", err.to_string()),
+                }
+            }
+        }
+    }
 }
 
 #[tokio::main]
-pub async fn main() {
+async fn main() {
     let args = Args::parse();
-    let method = args.method;
-    let first_arg = match method {
-        Method::Balance => args.first_arg.expect("Argument <Address> not provided"),
-        Method::Price => args.first_arg.expect("Argument <Amount> not provided"),
-        Method::Fund | Method::Withdraw => args.first_arg.expect("Argument <Amount> not provided"),
-        Method::Upload => args.first_arg.expect("Argument <File> not provided"),
-        _ => "".to_string(),
-    };
 
-    let wallet = match method {
-        Method::Balance => "".to_string(),
-        Method::Price => "".to_string(),
-        Method::Help => "".to_string(),
-        _ => args.wallet.expect("Argument <Wallet> not provided"),
-    };
-    let bundlr_url = args.host;
-    let timeout = args.timeout.unwrap_or_else(|| match method {
-        Method::Fund => DEFAULT_TIMEOUT_FUND,
-        _ => DEFAULT_TIMEOUT,
-    });
-    let currency = args.currency;
-
-    let (info, work): (
-        &str,
-        Pin<Box<dyn Future<Output = Result<String, BundlrError>>>>,
-    ) = match method {
-        Method::Balance => (
-            "Balance:",
-            Box::pin(run_balance(bundlr_url, &first_arg, &currency)),
-        ),
-        Method::Fund => {
-            let amount = u64::from_str_radix(&first_arg, 10).expect("Invalid amount");
-            (
-                "Fund:",
-                Box::pin(run_fund(amount, bundlr_url, &wallet, currency)),
-            )
-        }
-        Method::Withdraw => {
-            let amount = u64::from_str_radix(&first_arg, 10).expect("Invalid amount");
-            (
-                "Withdraw:",
-                Box::pin(run_withdraw(amount, bundlr_url, &wallet, currency)),
-            )
-        }
-        Method::Help => todo!("Method {:?} not implemented yet", method),
-        Method::Upload => {
-            let file = first_arg.to_string();
-            (
-                "Upload:",
-                Box::pin(run_upload(file, bundlr_url, &wallet, currency)),
-            )
-        }
-        Method::UploadDir => todo!("Method {:?} not implemented yet", method),
-        Method::Price => {
-            let amount = u64::from_str_radix(&first_arg, 10).expect("Invalid amount");
-            ("Price:", Box::pin(run_price(bundlr_url, currency, amount)))
-        }
-    };
-
-    match tokio::time::timeout(Duration::from_millis(timeout), work).await {
-        Ok(res) => match res {
-            Ok(ok) => println!("[Ok] {} {:?}", info, ok),
-            Err(err) => println!("[Err] {} {:?}", info, err),
-        },
-        Err(err) => println!("Error running task: {} {}", info, err.to_string()),
-    }
+    args.command.execute().await;
 }
